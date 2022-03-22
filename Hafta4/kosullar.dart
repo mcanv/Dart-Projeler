9b413e235f571f254ef90ec05c4c816ec1fb5c78
mcanv <mehmetcanvardar49@gmail.com>
2022-03-22 11:34:02 +0300

    i değerinin koşullanması oluşturuldu

--- Hafta4/kosullar.dart
+++ Hafta4/kosullar.dart
@@ -1,1 +1,10 @@
-
+void main(List<String> args) {
+  int i = 5;
+  if (i == 5) {
+    print("i değeri 5");
+  } else if (i == 6) {
+    print("i değeri 6");
+  } else {
+    print("i değeri 5 veya 6 değil");
+  }
+}
